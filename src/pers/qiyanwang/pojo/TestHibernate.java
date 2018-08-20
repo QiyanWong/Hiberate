dd101647342eaa92d21a4ade24d3611599de8093
QiyanWang <chicowang16@gmail.com>
2018-08-20 02:45:58 -0400

    one-to-many

--- src/pers/qiyanwang/pojo/TestHibernate.java
+++ src/pers/qiyanwang/pojo/TestHibernate.java
@@ -6,6 +6,8 @@ import org.hibernate.cfg.Configuration;
 
 import pers.qiyanwang.pojo.Product;
 
+import java.util.Set;
+
 public class TestHibernate {
   public static void main(String[] args) {
 
@@ -14,14 +16,14 @@ public class TestHibernate {
     Session s = sf.openSession();
     s.beginTransaction();
 
-    Product p = new Product();
-    p.setName("iphone7");
-    p.setPrice(7000);
-    s.save(p);
+    Category c = (Category) s.get(Category.class, 1);
+    Set<Product> ps = c.getProducts();
+    for (Product p : ps) {
+      System.out.println(p.getName());
+    }
 
     s.getTransaction().commit();
     s.close();
     sf.close();
   }
-
 }
